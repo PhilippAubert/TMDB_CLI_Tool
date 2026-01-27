ad44ae5237bfc80f8ddd07e563d8f7fdd09b2138
ujin22 <p.b.m.aubert@gmail.com>
2026-01-27 17:23:49 +0100

    Uninstall express and express types, prepare package.json and ts config to accept bin executable

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,28 @@
-import express from "express";
+#!/usr/bin/env node
+
 import dotenv from "dotenv";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 dotenv.config();
 
-const port = process.env["PORT"] || 4000;
-const accoundId = process.env["ACCOUNT_ID"] || 22709045;
+//const accoundId = process.env["ACCOUNT_ID"] || 22709045;
 
-const baseUrl = `https://api.themoviedb.org/3/account/${accoundId}/favorite/movies?language=en-US&page=1&sort_by=created_at.asc`;
+//const baseUrl = `https://api.themoviedb.org/3/account/${accoundId}/movie/popular?language=en-US&page=1';`;
+//const baseUrl2 = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+const baseUrl3 = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+// const baseUrl4 = "https://api.themoviedb.org/3/movie/top_rated";
+// const baseUrl5 = "https://api.themoviedb.org/3/movie/upcoming";
 
-const app = express();
 
-app.get("/", async (_req, res) => {
-    try {
-        const {data} = await axios({
-            method: "get",
-            url: baseUrl,
-            headers: {
-                accept: 'application/json',
-                Authorization:`Bearer ${process.env["API_TOKEN"]}`
-            }
-        });
-        return res.status(201).json({data});
-    } catch(e) {
-            if (e instanceof AxiosError) {
-                if (e.status === 401) {
-                    return res.status(401).json({e: "you're unauthorized!"});
-                }
-            }
-        return res.status(500).json({"error": e});
+console.log(process.argv);
+
+const {data} = await axios({
+    method: "get",
+    url: baseUrl3,
+    headers: {
+        accept: 'application/json',
+        Authorization:`Bearer ${process.env["API_TOKEN"]}`
     }
-})
+});
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+console.log(data);
