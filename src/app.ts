d24875d7df7ad683ab0b7a8904591dd7afdca5f8
ujin22 <p.b.m.aubert@gmail.com>
2026-01-29 19:55:59 +0100

    Use commander js and enable queries by type

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,41 @@
 #!/usr/bin/env node
 
 import dotenv from "dotenv";
+import program from "./commander.js";
 import axios from "axios";
 
 dotenv.config();
 
-//const accoundId = process.env["ACCOUNT_ID"] || 22709045;
+const categoryMap = {
+    popular : "popular",
+    top : "top_rated",
+    playing: "now_playing",
+    upcoming: "upcoming"
+};
 
-//const baseUrl = `https://api.themoviedb.org/3/account/${accoundId}/movie/popular?language=en-US&page=1';`;
-//const baseUrl2 = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-// const baseUrl3 = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-const baseUrl4 = "https://api.themoviedb.org/3/movie/top_rated";
-// const baseUrl5 = "https://api.themoviedb.org/3/movie/upcoming";
+const args = program.args;
 
 
-console.log(process.argv);
+const getCategory = (argValue:string[]) => {
+    const key = Object.keys(categoryMap).filter(item => item === argValue[0]);
+    return key ? categoryMap[key as unknown as keyof typeof categoryMap] : "No type specified";
+}
+
+const getUrl =  (category:string): string => {
+    return `https://api.themoviedb.org/3/movie/${category}?langauge=en-US&page=1`;
+}
+
+const category = getCategory(args);
+
+const urlFromQuery = getUrl(category);
 
 const {data} = await axios({
     method: "get",
-    url: baseUrl4,
+    url: urlFromQuery,
     headers: {
         accept: 'application/json',
         Authorization:`Bearer ${process.env["API_TOKEN"]}`
     }
 });
-data.results.forEach((item:any) => console.log(item.original_title));
+
+data.results.forEach((item:any) => console.log(item.original_title, item.title, item.release_date, item.original_language, item.vote_count));
---
+++ src/commander.ts
@@ -0,0 +1,12 @@
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("tmdb_cli_tool")
+    .description("CLI command to get shitty movies!")
+    .version("0.0.1");
+
+program.option("--type").argument("<string>");
+program.parse();
+
+export default program;
